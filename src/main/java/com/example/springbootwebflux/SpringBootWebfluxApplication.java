9de8a6c8baed890b0f2991e95cb5b5c7917b07fc
jcguerrero21 <jotaceguerrerom21@gmail.com>
2018-09-10 14:56:37 +0200

    Se añaden las clases DAO y los modelos y se añade inserción de productos a la base de datos mongoDB

--- src/main/java/com/example/springbootwebflux/SpringBootWebfluxApplication.java
+++ src/main/java/com/example/springbootwebflux/SpringBootWebfluxApplication.java
@@ -1,12 +1,40 @@
 package com.example.springbootwebflux;
 
+import com.example.springbootwebflux.models.dao.ProductoDao;
+import com.example.springbootwebflux.models.documents.Producto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootWebfluxApplication {
+public class SpringBootWebfluxApplication implements CommandLineRunner {
+
+    @Autowired
+    private ProductoDao productoDao;
+
+    private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebfluxApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Flux.just(new Producto("TV Panasonic Pantalla LCD", 456.89),
+                new Producto("Sony Camara HD Digital", 177.89),
+                new Producto("Apple iPod", 46.89),
+                new Producto("Sony Notebook", 846.89),
+                new Producto("Hewlett Packard Multifuncional", 200.89),
+                new Producto("Bianchi Bicileta", 70.89),
+                new Producto("HP Notebook Omen 17", 2500.89),
+                new Producto("Mica Cómoda 5 Cajones", 150.89),
+                new Producto("TV Sony Bravia OLED 4k Ultra HD", 2255.89)
+        )
+                .flatMap(producto -> productoDao.save(producto))
+                .subscribe(producto -> log.info("INSERT: " + producto.getId() + " " + producto.getNombre()));
     }
 }
---
+++ src/main/java/com/example/springbootwebflux/models/dao/ProductoDao.java
@@ -0,0 +1,10 @@
+package com.example.springbootwebflux.models.dao;
+
+import com.example.springbootwebflux.models.documents.Producto;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface ProductoDao extends ReactiveMongoRepository<Producto, String> {
+
+
+
+}
